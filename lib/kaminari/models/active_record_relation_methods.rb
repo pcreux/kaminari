f8413aea81c4f7d29bb0a3a572090df1df7b6149
Philippe Creux <pcreux@gmail.com>
2012-07-01 19:23:41 +0200

    Disable ActiveRecord DISTINCT + COUNT work around for ActiveRecord 3.2.

--- lib/kaminari/models/active_record_relation_methods.rb
+++ lib/kaminari/models/active_record_relation_methods.rb
@@ -19,7 +19,11 @@ module Kaminari
         # a workaround to count the actual model instances on distinct query because count + distinct returns wrong value in some cases. see https://github.com/amatsuda/kaminari/pull/160
         uses_distinct_sql_statement = c.to_sql =~ /DISTINCT/i
         if uses_distinct_sql_statement
-          c.length
+          if ActiveRecord::VERSION::STRING < '3.2'
+            c.length
+          else
+            c.count
+          end
         else
           # .group returns an OrderdHash that responds to #count
           c = c.count
